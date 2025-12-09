53d2df69172b5027873f13ceaa2ad13065c9abda
design7 <min.kim9914@gmail.com>
2025-12-09 10:22:21 +0900

    34

--- src/pages/DocumentManagement.tsx
+++ src/pages/DocumentManagement.tsx
@@ -116,7 +116,6 @@ export function DocumentManagement() {
     parentCategories,
     subcategories,
     documents,
-    addCategory,
     addSubcategory,
     fetchSubcategories,
     uploadDocument,
