d2f827a3e7f0e0855043c470f81cc134736e9c35
design7 <min.kim9914@gmail.com>
2026-05-06 15:56:53 +0900

    fix: NfcPlugin 명시적 등록 + onNewIntent NPE 방어처리

--- android/app/src/main/java/com/dms/app/MainActivity.java
+++ android/app/src/main/java/com/dms/app/MainActivity.java
@@ -1,17 +1,32 @@
 package com.dms.app;
 
 import android.content.Intent;
+import android.os.Bundle;
 import com.getcapacitor.BridgeActivity;
+import com.getcapacitor.PluginHandle;
 
 public class MainActivity extends BridgeActivity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        registerPlugin(NfcPlugin.class);
+        super.onCreate(savedInstanceState);
+    }
 
     @Override
     protected void onNewIntent(Intent intent) {
         super.onNewIntent(intent);
         setIntent(intent);
-        NfcPlugin nfcPlugin = (NfcPlugin) getBridge().getPlugin("NfcPlugin").getInstance();
-        if (nfcPlugin != null) {
-            nfcPlugin.handleNfcIntent(intent);
+        try {
+            PluginHandle handle = getBridge().getPlugin("NfcPlugin");
+            if (handle != null) {
+                NfcPlugin nfcPlugin = (NfcPlugin) handle.getInstance();
+                if (nfcPlugin != null) {
+                    nfcPlugin.handleNfcIntent(intent);
+                }
+            }
+        } catch (Exception e) {
+            android.util.Log.e("MainActivity", "NFC intent handling error", e);
         }
     }
 }
